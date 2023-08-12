7415947e959e0cddfbf011952c8ae4038f18d8b3
Prudhvi <prudhvinath1998@gmail.com>
2023-08-11 23:08:33 -0500

    Created Product Model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    productId = models.AutoField(primary_key=True)
+    sellerName = models.CharField(max_length=100)
+    productDescription = models.TextField()
+    pictureLocation = models.TextField()
+    brand = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.productId
