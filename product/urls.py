53776cccd9cfc81e685729d5bc93126c96ba7586
Prudhvi <prudhvinath1998@gmail.com>
2023-08-18 01:58:28 -0500

    Added images

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.filtered_products, name='filtered_products'),
 ]
+urlpatterns += staticfiles_urlpatterns()
