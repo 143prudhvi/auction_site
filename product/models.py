83d9f7195e488442647635e634559d2d66fd66c6
Prudhvi <prudhvinath1998@gmail.com>
2023-08-22 18:19:45 -0500

    Added Price field in Product

--- product/models.py
+++ product/models.py
@@ -8,6 +8,7 @@ class Product(models.Model):
     productDescription = models.TextField()
     pictureLocation = models.TextField()
     brand = models.CharField(max_length=50)
+    price = models.CharField(max_length=10,default="300")
 
     def __str__(self):
         return self.productId
