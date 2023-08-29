439c4db2119d1b405afbd4d445ce48c500d909d9
Prudhvi <prudhvinath1998@gmail.com>
2023-08-29 09:41:27 -0500

    Product Details

--- product/views.py
+++ product/views.py
@@ -13,8 +13,10 @@ def filtered_products(request):
     if seller:
         products = products.filter(sellerName=seller)
     product_images = {}
+    product_descriptions = {}
     for product in products:
         product_images[product.productId] = get_images_from_path("static/Images/" + product.pictureLocation)
+        product_descriptions[product.productId] = [feature for feature in product.productDescription.split("\n") if feature]
     distinct_brands = Product.objects.values_list('brand', flat=True).order_by('brand').distinct()
     distinct_sellers = Product.objects.values_list('sellerName', flat=True).order_by('sellerName').distinct()
     
@@ -24,7 +26,8 @@ def filtered_products(request):
         'distinct_sellers': distinct_sellers,
         'selected_brand': brand,
         'selected_seller': seller ,
-        'product_images': product_images
+        'product_images': product_images,
+        'product_descriptions': product_descriptions
     }
 
     return render(request, 'filtered_products.html', context)
